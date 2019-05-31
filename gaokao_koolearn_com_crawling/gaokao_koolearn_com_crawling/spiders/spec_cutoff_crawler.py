3974d37a4a48b63008555395e2628ccebe82616c
Zhaopeng Gong <zhaopeng@ZhaopengdeiMac.local>
2019-05-31 18:31:44 +0800

    ...

--- gaokao_koolearn_com_crawling/gaokao_koolearn_com_crawling/spiders/spec_cutoff_crawler.py
+++ gaokao_koolearn_com_crawling/gaokao_koolearn_com_crawling/spiders/spec_cutoff_crawler.py
@@ -7,77 +7,81 @@ import random
 class SpecCutoffCrawler(scrapy.Spider):
     name = 'spec_cutoff'
 
-    # def start_requests(self):
-    #     def generate_url(school, year):
-    #         base_url = \
-    #             "https://gaokao.koolearn.com/college/speline/0/0/" \
-    #                 f"{year}/{school}/0"
-    #         return base_url
-    #
-    #     _YEAR_DOMAIN = list(range(2008, 2018))
-    #
-    #     mongo_client = pymongo.MongoClient(self.settings.get('MONGO_URI'))
-    #     mongo_db = mongo_client[self.settings.get('MONGO_DATABASE')]
-    #     school_coll = mongo_db[self.settings.get('MONGO_COLLECTION_SCHOOL')]
-    #
-    #     self.sc_fail_link_coll = mongo_db[self.settings.get('MONGO_COLLECTION_SPEC_CUTOFF_FAIL_LINK')]
-    #
-    #     all_school = [s['_id'] for s in school_coll.find()]
-    #     random.shuffle(all_school)
-    #     # all_school = ['广东财经大学华商学院']
-    #
-    #     logging.info(f'Year Domain: {_YEAR_DOMAIN}')
-    #     logging.info(f'School Count: {len(all_school)}')
-    #     logging.info(f'School Domain: {all_school}')
-    #
-    #     for school in all_school:
-    #         year_domain = list(_YEAR_DOMAIN)
-    #         random.shuffle(year_domain)
-    #         for year in year_domain:
-    #             req_url = generate_url(school, year)
-    #
-    #             yield scrapy.Request(url=req_url, callback=self.parse)
-
     def start_requests(self):
+        def generate_url(school, year):
+            base_url = \
+                "https://gaokao.koolearn.com/college/speline/0/0/" \
+                    f"{year}/{school}/0"
+            return base_url
+
+        _YEAR_DOMAIN = list(range(2008, 2018))
+
         mongo_client = pymongo.MongoClient(self.settings.get('MONGO_URI'))
         mongo_db = mongo_client[self.settings.get('MONGO_DATABASE')]
+        school_coll = mongo_db[self.settings.get('MONGO_COLLECTION_SCHOOL')]
 
         self.sc_fail_link_coll = mongo_db[self.settings.get('MONGO_COLLECTION_SPEC_CUTOFF_FAIL_LINK')]
 
-        self.all_fail_links = set([fl['_id'] for fl in self.sc_fail_link_coll.find()])
+        all_school = [s['_id'] for s in school_coll.find()]
+        random.shuffle(all_school)
+        # all_school = ['广东财经大学华商学院']
 
-        logging.info(f'Fail Link Count: {len(self.all_fail_links)}')
+        logging.info(f'Year Domain: {_YEAR_DOMAIN}')
+        logging.info(f'School Count: {len(all_school)}')
+        logging.info(f'School Domain: {all_school}')
 
-        fail_links = set(self.all_fail_links)
-        while len(fail_links) > 0:
-            for fl in fail_links:
-                yield scrapy.Request(url=fl, callback=self.parse)
-            fail_links = set(self.all_fail_links)
+        for school in all_school:
+            year_domain = list(_YEAR_DOMAIN)
+            random.shuffle(year_domain)
+            for year in year_domain:
+                req_url = generate_url(school, year)
+
+                yield scrapy.Request(url=req_url, callback=self.parse)
+
+    # def start_requests(self):
+    #     mongo_client = pymongo.MongoClient(self.settings.get('MONGO_URI'))
+    #     mongo_db = mongo_client[self.settings.get('MONGO_DATABASE')]
+    #
+    #     self.sc_fail_link_coll = mongo_db[self.settings.get('MONGO_COLLECTION_SPEC_CUTOFF_FAIL_LINK')]
+    #
+    #     self.all_fail_links = set([fl['_id'] for fl in self.sc_fail_link_coll.find()])
+    #
+    #     logging.info(f'Fail Link Count: {len(self.all_fail_links)}')
+    #
+    #     fail_links = set(self.all_fail_links)
+    #     while len(fail_links) > 0:
+    #         for fl in fail_links:
+    #             yield scrapy.Request(url=fl, callback=self.parse)
+    #         fail_links = set(self.all_fail_links)
 
     def parse(self, response):
         pb60 = response.css('div.wrap div#content div.mt20 div.fl div.pb60')
 
-        if len(pb60) > 0:
-            try:
-                self.sc_fail_link_coll.delete_one({'_id': response.url})
-            except Exception:
-                pass
-            try:
-                self.all_fail_links.remove(response.url)
-            except Exception:
-                pass
-        else:
-            try:
-                self.sc_fail_link_coll.insert_one({'_id': response.url})
-            except Exception:
-                pass
-            try:
-                self.all_fail_links.add(response.url)
-            except Exception:
-                pass
+        go_next = False
 
         try:
             school_table = list(pb60.css('table.tab2 tr'))[1:]
+
+            if len(school_table) > 0:
+                go_next = True
+                try:
+                    self.sc_fail_link_coll.delete_one({'_id': response.url})
+                except Exception:
+                    pass
+                try:
+                    self.all_fail_links.remove(response.url)
+                except Exception:
+                    pass
+            else:
+                try:
+                    self.sc_fail_link_coll.insert_one({'_id': response.url})
+                except Exception:
+                    pass
+                try:
+                    self.all_fail_links.add(response.url)
+                except Exception:
+                    pass
+
             for school_item in school_table:
                 try:
                     school_tds = school_item.css('td')
@@ -99,9 +103,10 @@ class SpecCutoffCrawler(scrapy.Spider):
         except Exception:
             pass
 
-        try:
-            pages = list(pb60.css('div.page a'))
-            next_page = [p.css('::attr(href)').get() for p in pages if p.css('::text').get().strip() == '下一页'][0]
-            yield response.follow(url=next_page, callback=self.parse)
-        except Exception:
-            pass
+        if go_next:
+            try:
+                pages = list(pb60.css('div.page a'))
+                next_page = [p.css('::attr(href)').get() for p in pages if p.css('::text').get().strip() == '下一页'][0]
+                yield response.follow(url=next_page, callback=self.parse)
+            except Exception:
+                pass
