89f0bdcbe4de8d987fb4ec42dcbcc60406cc4778
Zhaopeng Gong <zhaopeng@ZhaopengdeiMac.local>
2019-06-06 11:16:03 +0800

    ...

--- intent-pt/src/main/java/com/benben/pt/intent/configuration/RestTemplateConfiguration.java
+++ intent-pt/src/main/java/com/benben/pt/intent/configuration/RestTemplateConfiguration.java
@@ -1,8 +1,13 @@
 package com.benben.pt.intent.configuration;
 
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,9 +18,20 @@ public class RestTemplateConfiguration {
 
     @Bean
     @Primary
-    public RestTemplate restTemplate() {
+    public RestTemplate restTemplate(
+            RestTemplateBuilder builder) throws Exception {
 
-        final var restTemplate = new RestTemplate();
+        final var sslContext = SSLContextBuilder.create()
+                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+                .build();
+
+        final var client = HttpClients.custom()
+                .setSSLContext(sslContext)
+                .build();
+
+        final var restTemplate = builder.requestFactory(() ->
+                new HttpComponentsClientHttpRequestFactory(client))
+                .build();
 
         setDefaultCharsetOfStringHttpMessageConverter(restTemplate,
                 Charset.forName("UTF-8"));
